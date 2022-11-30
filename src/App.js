dabd28e618e536a2204f7302978fe04bd0375597
Tetiana Matviienko <tanya.matvienko.90@gmail.com>
2022-11-30 12:12:29 +0000

    Add Footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import './App.css';
 
 export default function App() {
   return (
-    <div>Weather App
+    <div className='App'>
+      Weather App
+      <footer>
+        This project was coded by Tetiana Matviienko and is {" "}
+        <a href='https://github.com/MomTanya/react-weather-22' target="_blank">open-soursed on GitHub</a>
+      </footer>
     </div>);
 }
 
