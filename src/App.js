fb0882e278e21989d3504be0c05e77d421c01092
Tetiana Matviienko <tanya.matvienko.90@gmail.com>
2022-11-30 12:27:53 +0000

    Add link to personal web-site

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className='App'>
-      Weather App
+     <h1> Weather App</h1>
       <footer>
-        This project was coded by Tetiana Matviienko and is {" "}
-        <a href='https://github.com/MomTanya/react-weather-22' target="_blank">open-soursed on GitHub</a>
+        This project was coded by {" "} 
+        <a href='https://jocular-choux-52b0c5.netlify.app/' target="_blank" rel="noreferrer">Tetiana Matvienko</a>{" "}and is{" "}
+        <a href='https://github.com/MomTanya/react-weather-22' target="_blank" rel="noreferrer">open-soursed on GitHub</a>
       </footer>
     </div>);
 }
