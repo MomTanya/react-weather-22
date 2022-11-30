27f4e25f1f48f596199089ac80e6ca058eb12b19
Tetiana Matviienko <tanya.matvienko.90@gmail.com>
2022-11-30 13:52:28 +0000

    add bootstrap in project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className='App'>
+      <div className='container'>
      <h1> Weather App</h1>
       <footer>
         This project was coded by {" "} 
         <a href='https://jocular-choux-52b0c5.netlify.app/' target="_blank" rel="noreferrer">Tetiana Matvienko</a>{" "}and is{" "}
         <a href='https://github.com/MomTanya/react-weather-22' target="_blank" rel="noreferrer">open-soursed on GitHub</a>
       </footer>
-    </div>);
+      </div>
+    </div>
+    );
 }
 
